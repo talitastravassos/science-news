fe9f4ffecb5cb04d5901289f2823c8c284d3419e
talitast <talitatravassos.tt@gmail.com>
2019-11-10 15:41:17 -0300

    style: news card grid

--- src/components/NewsCard/index.tsx
+++ src/components/NewsCard/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import styles from './styles.module.scss';
+import format from "date-fns/format";
+import styles from "./styles.module.scss";
 import { News } from "../../context/news.types";
 interface Props {
   post: News;
@@ -8,7 +9,20 @@ interface Props {
 export default function NewsCard({ post }: Props) {
   return (
     <div className={styles.card}>
-      <h1>{post.title}</h1>
+      <img src={post.image} alt={post.title} className={styles.image}/>
+
+      <div className={styles.text_container}>
+        <p>{post.category}</p>
+        <h1>{post.title}</h1>
+        <p>{post.summary}</p>
+        <div className={styles.flex_container}>
+          <p>by: {post.author}</p>
+          <p>{format(new Date(post.datetime), "MMMM dd, yyyy")}</p>
+        </div>
+        <a href={post.url} target="_blank" rel="noopener noreferrer">
+          Read all
+        </a>
+      </div>
     </div>
   );
 }
